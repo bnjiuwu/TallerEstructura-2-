92a9ebe3c06c3c717ce6ac6a32cfaa09990383c4
ClaudioMonsalv <claudiomonsalve1287@gmail.com>
2024-12-09 21:41:55 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,202 +1,363 @@
 #include <iostream>
 #include <vector>
-#include <limits>
 using namespace std;
 
 // Clase Tablero
-class Tablero {
-private:
-    vector<char> board;
-    int moves;
+class Tablero 
+{
+    private:
+        vector<char> tablero;
+        int movimientos;
 
-public:
-    Tablero() : board(10), moves(0) {
-        for (int i = 1; i <= 9; ++i) board[i] = '0' + i;
-    }
-
-    void mostrar() {
-        cout << "\n   |   |   \n";
-        cout << " " << board[1] << " | " << board[2] << " | " << board[3] << "\n";
-        cout << "___|___|___\n";
-        cout << "   |   |   \n";
-        cout << " " << board[4] << " | " << board[5] << " | " << board[6] << "\n";
-        cout << "___|___|___\n";
-        cout << "   |   |   \n";
-        cout << " " << board[7] << " | " << board[8] << " | " << board[9] << "\n";
-        cout << "   |   |   \n";
-    }
-
-    bool mover(int posicion, char marca) {
-        if (posicion < 1 || posicion > 9 || board[posicion] != '0' + posicion) {
-            return false;
+    public:
+        Tablero() : tablero(10), movimientos(0) 
+        {
+            for (int i = 1; i <= 9; ++i)
+            {
+                tablero[i] = '0' + i;
+            }
         }
-        board[posicion] = marca;
-        moves++;
-        return true;
-    }
 
-    int verificarEstado() {
-        for (int i = 1; i <= 7; i += 3) {
-            if (board[i] == board[i + 1] && board[i + 1] == board[i + 2]) return board[i] == 'X' ? 1 : -1;
+        void mostrar() 
+        {
+            cout << "\n   |   |   \n";
+            cout << " " << tablero[1] << " | " << tablero[2] << " | " << tablero[3] << "\n";
+            cout << "___|___|___\n";
+            cout << "   |   |   \n";
+            cout << " " << tablero[4] << " | " << tablero[5] << " | " << tablero[6] << "\n";
+            cout << "___|___|___\n";
+            cout << "   |   |   \n";
+            cout << " " << tablero[7] << " | " << tablero[8] << " | " << tablero[9] << "\n";
+            cout << "   |   |   \n";
         }
-        for (int i = 1; i <= 3; ++i) {
-            if (board[i] == board[i + 3] && board[i + 3] == board[i + 6]) return board[i] == 'X' ? 1 : -1;
+
+        bool mover(int posicion, char marca) 
+        {
+            if (posicion < 1 || posicion > 9 || tablero[posicion] != '0' + posicion) 
+            {
+                return false;
+            }
+            tablero[posicion] = marca;
+            movimientos++;
+            return true;
         }
-        if (board[1] == board[5] && board[5] == board[9]) return board[1] == 'X' ? 1 : -1;
-        if (board[3] == board[5] && board[5] == board[7]) return board[3] == 'X' ? 1 : -1;
-        return (moves == 9) ? 0 : -2;
-    }
 
-    void reiniciar() {
-        for (int i = 1; i <= 9; ++i) board[i] = '0' + i;
-        moves = 0;
-    }
-
-    vector<int> movimientosDisponibles() {
-        vector<int> disponibles;
-        for (int i = 1; i <= 9; ++i) {
-            if (board[i] == '0' + i) disponibles.push_back(i);
+        int verificarEstado() 
+        {
+            for (int i = 1; i <= 7; i += 3) 
+            {
+                if (tablero[i] == tablero[i + 1] && tablero[i + 1] == tablero[i + 2])
+                {
+                    if (tablero[i] == 'X') 
+                    {
+                        return 1;
+                    } 
+                    else 
+                    {
+                        return -1;
+                    }
+                }
+            }
+            for (int i = 1; i <= 3; ++i) 
+            {
+                if (tablero[i] == tablero[i + 3] && tablero[i + 3] == tablero[i + 6]) 
+                {
+                    if (tablero[i] == 'X') 
+                    {
+                        return 1;
+                    } 
+                    else 
+                    {
+                        return -1;
+                    }
+                }
+            }
+            if (tablero[1] == tablero[5] && tablero[5] == tablero[9])
+            {
+                if (tablero[1] == 'X') 
+                    {
+                        return 1;
+                    } 
+                    else 
+                    {
+                        return -1;
+                    }
+            }
+            if (tablero[3] == tablero[5] && tablero[5] == tablero[7])
+            {
+                if (tablero[3] == 'X') 
+                    {
+                        return 1;
+                    } 
+                    else 
+                    {
+                        return -1;
+                    }
+            }
+            if (movimientos == 9) 
+            {
+                return 0;
+            } 
+            else 
+            {
+                return -2;
+            }
         }
-        return disponibles;
-    }
 
-    void deshacerMovimiento(int posicion) {
-        board[posicion] = '0' + posicion;
-        moves--;
-    }
+        void reiniciar() 
+        {
+            for (int i = 1; i <= 9; ++i)
+            {
+                tablero[i] = '0' + i;
+            }
+            movimientos = 0;
+        }
 
-    char obtenerCasilla(int posicion) {
-        return board[posicion];
-    }
+        vector<int> movimientosDisponibles() 
+        {
+            vector<int> disponibles;
+            for (int i = 1; i <= 9; ++i) 
+            {
+                if (tablero[i] == '0' + i)
+                {
+                    disponibles.push_back(i);
+                }
+            }
+            return disponibles;
+        }
+
+        void deshacerMovimiento(int posicion) 
+        {
+            tablero[posicion] = '0' + posicion;
+            movimientos--;
+        }
+
+        char obtenerCasilla(int posicion) 
+        {
+            return tablero[posicion];
+        }
 };
 
 // Algoritmo Minimax
-int minimax(Tablero &tablero, bool esMax, int profundidad = 0) {
+int minimax(Tablero &tablero, bool esMax, int profundidad = 0) 
+{
     int estado = tablero.verificarEstado();
-    if (estado != -2) return estado;
-
-    if (esMax) {
-        int mejor = numeric_limits<int>::min();
-        for (int mov : tablero.movimientosDisponibles()) {
-            tablero.mover(mov, 'X');
-            mejor = max(mejor, minimax(tablero, false, profundidad + 1));
-            tablero.deshacerMovimiento(mov);
-        }
-        return mejor;
-    } else {
-        int peor = numeric_limits<int>::max();
-        for (int mov : tablero.movimientosDisponibles()) {
-            tablero.mover(mov, 'O');
-            peor = min(peor, minimax(tablero, true, profundidad + 1));
-            tablero.deshacerMovimiento(mov);
-        }
-        return peor;
+    if (estado != -2)
+    { 
+        return estado;
     }
+    int mejor;
+    if (esMax) 
+    {
+        mejor = -9999;
+    } 
+    else 
+    {
+        mejor = 9999;
+    }  
+    for (int mov : tablero.movimientosDisponibles()) 
+    {
+        char marca;
+        if (esMax)  
+        {
+            marca = 'X';
+        } 
+        else    
+        {
+            marca = 'O';
+        }
+        tablero.mover(mov, marca);
+        int evaluacion = minimax(tablero, !esMax, profundidad + 1);  
+        if (esMax) 
+        {
+            mejor = max(mejor, evaluacion);
+        } 
+        else 
+        {
+            mejor = min(mejor, evaluacion);
+        }
+        tablero.deshacerMovimiento(mov); 
+    }
+    return mejor;
 }
 
 // Algoritmo Minimax con poda Alfa-Beta
-int minimaxAlfaBeta(Tablero &tablero, bool esMax, int alfa, int beta, int profundidad = 0) {
+int minimaxAlfaBeta(Tablero &tablero, bool esMax, int alfa, int beta, int profundidad = 0) 
+{
     int estado = tablero.verificarEstado();
-    if (estado != -2) return estado;
-
-    if (esMax) {
-        int mejor = numeric_limits<int>::min();
-        for (int mov : tablero.movimientosDisponibles()) {
-            tablero.mover(mov, 'X');
-            mejor = max(mejor, minimaxAlfaBeta(tablero, false, alfa, beta, profundidad + 1));
-            tablero.deshacerMovimiento(mov);
-            alfa = max(alfa, mejor);
-            if (beta <= alfa) break;
-        }
-        return mejor;
-    } else {
-        int peor = numeric_limits<int>::max();
-        for (int mov : tablero.movimientosDisponibles()) {
-            tablero.mover(mov, 'O');
-            peor = min(peor, minimaxAlfaBeta(tablero, true, alfa, beta, profundidad + 1));
-            tablero.deshacerMovimiento(mov);
-            beta = min(beta, peor);
-            if (beta <= alfa) break;
-        }
-        return peor;
+    if (estado != -2)
+    {
+        return estado;
     }
+    int mejor;
+    if (esMax) 
+    {
+        mejor = -9999;
+    } 
+    else 
+    {
+        mejor = 9999;
+    } 
+
+    for (int mov : tablero.movimientosDisponibles()) 
+    {
+        if (esMax) 
+        {
+            tablero.mover(mov, 'X');
+        } 
+        else 
+        {
+            tablero.mover(mov, 'O');
+        }       
+        int evaluacion = minimaxAlfaBeta(tablero, !esMax, alfa, beta, profundidad + 1); 
+        if (esMax) 
+        {
+            mejor = max(mejor, evaluacion);  
+            alfa = max(alfa, mejor);  
+        } 
+        else 
+        {
+            mejor = min(mejor, evaluacion);  
+            beta = min(beta, mejor);  
+        }
+        if (beta <= alfa) 
+        {
+            tablero.deshacerMovimiento(mov);  
+            break;
+        }
+        tablero.deshacerMovimiento(mov);  
+    }
+    return mejor;
 }
 
 // Función para obtener el mejor movimiento con un algoritmo
-int obtenerMejorMovimiento(Tablero &tablero, bool usarPoda) {
-    int mejorValor = numeric_limits<int>::min();
+int obtenerMejorMovimiento(Tablero &tablero, bool usarPoda) 
+{
+    int mejorValor = -9999;  
     int mejorMovimiento = -1;
 
-    for (int mov : tablero.movimientosDisponibles()) {
+    for (int mov : tablero.movimientosDisponibles()) 
+    {
         tablero.mover(mov, 'X');
-        int valor = usarPoda ? minimaxAlfaBeta(tablero, false, numeric_limits<int>::min(), numeric_limits<int>::max())
-                             : minimax(tablero, false);
+        int valor;
+        if (usarPoda) 
+        {
+            valor = minimaxAlfaBeta(tablero, false, -9999, 9999);
+        } 
+        else 
+        {
+            valor = minimax(tablero, false, 0);
+        }
         tablero.deshacerMovimiento(mov);
 
-        if (valor > mejorValor) {
+        if (valor > mejorValor) 
+        {
             mejorValor = valor;
             mejorMovimiento = mov;
         }
     }
-
     return mejorMovimiento;
 }
 
-// Modo de dos jugadores
-void modoDosJugadores(Tablero &tablero) {
+
+void modoDosJugadores(Tablero &tablero) 
+{
     int jugador = 1, posicion, estado;
     char marca;
 
     do {
         tablero.mostrar();
-        marca = (jugador == 1) ? 'X' : 'O';
+        if (jugador == 1) 
+        {
+            marca = 'X';
+        } 
+        else 
+        {
+            marca = 'O';
+        }
         cout << "Jugador " << jugador << ", ingresa tu movimiento (1-9): ";
         cin >> posicion;
 
-        if (!tablero.mover(posicion, marca)) {
+        if (!tablero.mover(posicion, marca)) 
+        {
             cout << "Movimiento inválido, intenta de nuevo.\n";
             continue;
         }
 
         estado = tablero.verificarEstado();
-        if (estado != -2) break;
+        if (estado != -2)
+        { 
+            break;
+        }
         jugador = 3 - jugador;
     } while (true);
 
     tablero.mostrar();
-    if (estado == 0) cout << "¡Es un empate!\n";
-    else cout << "¡Felicidades! El Jugador " << ((estado == 1) ? 1 : 2) << " gana.\n";
+    if (estado == 0)
+    {
+        cout << "¡Es un empate!\n";
+    }
+    else
+    {
+        int jugador;
+        if (estado == 1)
+        {
+            jugador = 1;
+        } 
+        else 
+        {
+            jugador = 2;
+        }
+        cout << "¡Felicidades! El Jugador " << jugador << " gana.\n";
+    }
 }
 
 // Modo IA contra el jugador
-void modoContraIA(Tablero &tablero, bool usarPoda) {
+void modoContraIA(Tablero &tablero, bool usarPoda) 
+{
     int jugador = 1, posicion, estado;
     char marca;
 
     do {
         tablero.mostrar();
-        if (jugador == 1) {
+        if (jugador == 1) 
+        {
             cout << "Jugador, ingresa tu movimiento (1-9): ";
             cin >> posicion;
-            if (!tablero.mover(posicion, 'O')) {
+            if (!tablero.mover(posicion, 'O')) 
+            {
                 cout << "Movimiento inválido, intenta de nuevo.\n";
                 continue;
             }
-        } else {
+        } 
+        else 
+        {
             int mejorMovimiento = obtenerMejorMovimiento(tablero, usarPoda);
             tablero.mover(mejorMovimiento, 'X');
             cout << "La IA juega en la posición " << mejorMovimiento << ".\n";
         }
 
         estado = tablero.verificarEstado();
-        if (estado != -2) break;
+        if (estado != -2)
+        { 
+            break;
+        }
         jugador = 3 - jugador;
     } while (true);
 
     tablero.mostrar();
-    if (estado == 0) cout << "¡Es un empate!\n";
-    else if (estado == 1) cout << "¡La IA gana!\n";
-    else cout << "¡Felicidades! Has ganado.\n";
+    if (estado == 0)
+    {
+        cout << "¡Es un empate!\n";
+    }
+    else if (estado == 1)
+    {
+        cout << "¡La IA gana!\n";
+    }
+    else
+    {
+        cout << "¡Felicidades! Has ganado.\n";
+    }
 }
 
 // Menú principal
